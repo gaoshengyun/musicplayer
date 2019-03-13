52bc3d85df75740a96e2187e04e24e83c522ea87
gaoshengyun <18616242909@163.com>
2019-03-13 16:55:29 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    helo world
+  </div>
+</template>
+
+<script type="text/ecmasript-6">
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable";
+
+  #app
+    color:$colot-theme
+</style>
