d42968afcfe1540c187d218b017f4a05a54e0cff
gaoshengyun <18616242909@163.com>
2019-03-14 14:38:37 +0800

    配置路由引入组件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
 //解决300ms延迟问题
 import  fastclick from 'fastclick'
 fastclick.attach(document.body)
@@ -11,6 +11,6 @@ import 'common/stylus/index.styl'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  router,
   render:h => h(App),
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Recommend from '@/components/recommend/recommend'
+import Singer from '@/components/singer/singer'
+import Search from '@/components/search/search'
+import Rank from '@/components/rank/rank'
 
 
 Vue.use(Router)
@@ -7,7 +11,24 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-     
+      path:'/',
+      redirect:'/recommend'
+    },
+    {
+      path:'/recommend',
+      component:Recommend
+    },
+    {
+      path:'/singer',
+      component:Singer
+    },
+    {
+      path:'/rank',
+      component:Rank
+    },
+    {
+      path:'/search',
+      component:Search
     }
   ]
 })
