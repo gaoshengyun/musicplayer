57712b698258c1cdf85ab3b591693eb45bab5351
gaoshengyun <18616242909@163.com>
2019-03-15 16:02:16 +0800

    轮播组件

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot></slot>
+    </div>
+    <div class="dots">
+      <span class="dot" v-for="(item, index) in dots" :key="index" :class="{active:currentPageIndex === index}"></span>
+    </div>
+  </div>
+</template>
+<script>
+import BScroll from 'better-scroll'
+import {addClass} from '@/common/js/dom.js'
+
+export default {
+  data() {
+    return {
+      dots:[],
+      currentPageIndex:0
+    }
+  },
+  props:{
+    loop:{
+      type:Boolean,
+      default:true
+    },
+    autoPlay:{
+      type:Boolean,
+      default:true
+    },
+    interval:{
+      type: Number,
+      default:4
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.setSlideWidth()
+      this.initDots()
+      this.initSlider()
+      if(this.autoPlay){
+        this.play()
+      }
+    },20)
+  },
+  methods: {
+    setSlideWidth(){
+      this.children = this.$refs.sliderGroup.children
+      let width = 0
+      let sliderWidth = this.$refs.slider.clientWidth
+      for(let i=0;i<this.children.length;i++){
+        let child = this.children[i]
+        addClass(child,'slider-item')
+        child.style.width = sliderWidth + 'px'
+        width += sliderWidth
+      }
+
+      if(this.loop){
+        width += 2 * sliderWidth
+      }
+      this.$refs.sliderGroup.style.width = width + 'px'
+    },
+    initSlider(){
+      this.slider = new BScroll(this.$refs.slider,{
+        scrollX:true,
+        scrollY:false,
+        momentum:false,
+        snap: {
+            loop: this.loop
+          }
+      })
+      this.slider.on('scrollEnd',()=>{
+        this.currentPageIndex = this.slider.getCurrentPage().pageX
+
+        if(this.autoPlay){
+          clearTimeout(this.timer)
+          this.play()
+        }
+      })
+    },
+    play(){
+      let pageIndex = this.currentPageIndex + 1
+      this.timer = setTimeout(() =>{
+        this.slider.goToPage(pageIndex,0,400)
+      },this.interval)
+    },
+    initDots(){
+      this.dots = new Array(this.children.length)
+    }
+  },
+}
+</script>
+<style lang="stylus" scoped>
+@import '~common/stylus/variable'
+.slider
+  min-height 1px
+  position relative
+  overflow hidden
+  .slide-group
+    position relative
+    overflow hidden
+    white-space nowrap
+    .slide-item
+      float left
+      box-sizing border-box
+      overflow hidden
+      text-align center
+      a
+        display block
+        width 100%
+        overflow hidden
+        text-decoration none
+      img
+        display block
+        width 100%
+  .dots
+    position absolute
+    right 0
+    left 0
+    bottom 12px
+    text-align center
+    font-size 0
+    overflow hidden
+    .dot
+      display inline-block
+      margin 0 4px
+      width 8px
+      height 8px
+      border-radius 50%
+      background $color-text-l
+      &.active
+        width 20px
+        border-radius 5px
+        background $color-text-ll
+</style>
