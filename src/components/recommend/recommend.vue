369f394059ecbad9dc800fc93f9703bcef8b8783
gaoshengyun <18616242909@163.com>
2019-03-15 16:03:11 +0800

    引用轮播组件

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slide-wrap"></div>
+      <div v-if="recommends.length" class="slide-wrap">
+        <slider>
+          <div v-for="(item, index) in recommends" :key="item.id">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl" alt="">
+            </a>
+          </div>
+        </slider>
+      </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
       </div>
@@ -9,10 +17,11 @@
   </div>
 </template>
 <script>
+import Slider from '@/base/slider/slider'
 export default {
    data() {
     return {
-      
+      recommends:[]
     }
   },
   created() {
@@ -22,59 +31,55 @@ export default {
     getRecommend(){
       this.axios.get('/proxy/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg?_=1552550604499&g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8&notice=0&platform=h5&needNewCode=1').then(result => {
         if (result.status === 200) {
-          console.log(result.data.data.slider)
+          this.recommends = result.data.data.slider
+          //console.log(result.data.data.slider)
         }
       })
     }
   },
+  components:{
+    Slider
+  }
 }
 </script>
-<style lang="stylus" scoped>
-@import '~common/stylus/variable'
-.recommend
-  position fixed
-  width 100%
-  top 88px
-  bottom 0
-  .recommend-content
-    height 100%
-    overflow hidden
-    .slide-wrap
-      position relative
-      width 100%
-      overflow hidden
-    .recommend-list
-      .list-title
-        height 65px
-        line-height 65px
-        text-align center
-        font-size $font-size-medium
-        color $color-theme
-      .item
-        display flex
-        box-sizing border-box
-        align-items center
-        padding 0 20px 20px 20px
-        .icon
-          flex 0 0 60px
-          width 60px
-          padding-right 20px
-        .text
-          display flex
-          flex-direction column
-          justify-content center
-          flex 1
-          line-height 20px
-          overflow hidden
-          font-size $font-size-medium
-          .name
-            margin-bottom 10px
-            color $color-text
-          .desc
-            color $color-text
-    .loading-container
-      position absolute
-      width 100%
-      top 50%
-      transform translateY(-50%)
+<style scoped lang="stylus">
+  @import "~common/stylus/variable"
+
+  .slider
+    min-height: 1px
+    .slider-group
+      position: relative
+      overflow: hidden
+      white-space: nowrap
+      .slider-item
+        float: left
+        box-sizing: border-box
+        overflow: hidden
+        text-align: center
+        a
+          display: block
+          width: 100%
+          overflow: hidden
+          text-decoration: none
+        img
+          display: block
+          width: 100%
+    .dots
+      position: absolute
+      right: 0
+      left: 0
+      bottom: 12px
+      text-align: center
+      font-size: 0
+      .dot
+        display: inline-block
+        margin: 0 4px
+        width: 8px
+        height: 8px
+        border-radius: 50%
+        background: $color-text-l
+        &.active
+          width: 20px
+          border-radius: 5px
+          background: $color-text-ll
 </style>
