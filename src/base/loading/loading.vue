a9f7b54654b19807c0d3ac6eda43c9dbbfcb10dc
gaoshengyun <18616242909@163.com>
2019-03-18 16:26:02 +0800

    loading组件

---
+++ src/base/loading/loading.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="loading">
+    <img src="./loading.gif" alt="" width="24px">
+    <p class="desc">{{title}}</p>
+  </div>
+</template>
+<script>
+export default {
+  props:{
+    title:{
+      type:String,
+      default:'载入中'
+    }
+  }
+}
+</script>
+<style lang="stylus">
+@import "~common/stylus/variable"
+.loading 
+  width 100%
+  text-align center
+  .desc
+    line-height 20px
+    font-size $font-size-small
+    color $color-text-l
+</style>
